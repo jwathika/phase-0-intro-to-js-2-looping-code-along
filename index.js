b960417f3e13fe37d01c3b2db31f83cd5cc5d01c
wathika-eng <wathika02@gmail.com>
2024-03-17 23:01:36 +0300

    confusing

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+function writeCards(names, message) {
+	function countDown(number) {
+		let countdownMessages = [];
+		for (let i = number; i >= 0; i--) {
+			countdownMessages.push(i);
+		}
+		return countdownMessages;
+	}
+
+	let thankYouMessages = [];
+	let i = 0;
+	for (i; i < names.length; i++) {
+		let countdownNumbers = countDown(names[i].length);
+		console.log(countdownNumbers.join('... '));
+		thankYouMessages.push(
+			`Thank you, ${names[i]}, for the wonderful ${message} gift!`
+		);
+	}
+
+	return thankYouMessages;
+}
